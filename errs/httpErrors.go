d965d77c5dd2048ebe2d4efd9b2c8bcbb2f9cb2d
Moshood Adepitan <mkoabiola95@gmail.com>
2024-06-06 23:28:47 +0100

    refactored http errors file

--- errs/httpErrors.go
+++ errs/httpErrors.go
@@ -1,4 +1,4 @@
-package errors
+package errs
 
 import "net/http"
 
@@ -17,4 +17,12 @@ func ErrUnprocessableEntity(err error) *ErrHttpRequest {
 
 func ErrResourceNotFound(err error) *ErrHttpRequest {
 	return &ErrHttpRequest{err: err, StatusCode: http.StatusNotFound}
+}
+
+func ErrInternalServerError(err error) *ErrHttpRequest {
+	return &ErrHttpRequest{err: err, StatusCode: http.StatusInternalServerError}
+}
+
+func ErrAccessDenied(err error) *ErrHttpRequest {
+	return &ErrHttpRequest{err: err, StatusCode: http.StatusForbidden}
 }
